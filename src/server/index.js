cbaffa8bcb95b82889f00420a54010b94dc6a5b9
David Soto <davidsotosendarrubias@gmail.com>
2022-02-21 12:26:06 +0100

    fseG

--- src/server/index.js
+++ src/server/index.js
@@ -5,6 +5,7 @@ const express = require("express");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const { notFoundError, generalError } = require("./middlewares/errors");
+const kindsRouter = require("./routers/kindsRouter");
 
 const app = express();
 
@@ -21,7 +22,10 @@ const startServer = (port) =>
   });
 
 app.use(morgan("dev"));
+app.use(express.json());
 app.use(helmet());
+
+app.use("/kinds", kindsRouter);
 
 app.use(notFoundError);
 app.use(generalError);

5aa9196cebe3275cd748c8a7c229d583d1d2936a
David Soto <davidsotosendarrubias@gmail.com>
2022-02-21 12:37:00 +0100

    fsdug

--- src/server/controllers/kindsControllers.js
+++ src/server/controllers/kindsControllers.js
@@ -9,4 +9,19 @@ const listKinds = async (req, res, next) => {
   }
 };
 
+const getKind = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const kind = await Kind.findById(id);
+    if (kind) {
+      res.json(kind);
+      return;
+    }
+    const error = new Error("kind not found");
+    error.code = 404;
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = { listKinds };
